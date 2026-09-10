bd8b425782f66151bb87cc6b75e07d54479f40d3
andersondahsilva05-crypto <anderson.dah.silva05@gmail.com>
2026-09-10 15:14:07 -0300

    corrige bloqueio do modal e restaura clique fora

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -58,7 +58,7 @@ DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content> & { hideCloseButton?: boolean }
->(({ className, children, hideCloseButton = false, onInteractOutside, onEscapeKeyDown, ...props }, ref) => {
+>(({ className, children, hideCloseButton = false, ...props }, ref) => {
   const mounted = useIsMounted();
 
   if (!mounted) {
@@ -70,14 +70,6 @@ const DialogContent = React.forwardRef<
       <DialogOverlay />
       <DialogPrimitive.Content
         ref={ref}
-        onInteractOutside={(event) => {
-          event.preventDefault();
-          onInteractOutside?.(event);
-        }}
-        onEscapeKeyDown={(event) => {
-          event.preventDefault();
-          onEscapeKeyDown?.(event);
-        }}
         className={cn(
           "fixed left-[50%] top-[50%] z-50 grid max-h-[90vh] w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 overflow-y-auto border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 sm:rounded-lg",
           className,
